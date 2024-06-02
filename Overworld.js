47627e3b2e0a2345dd9caa3b0670a9bb43f8bb33
Archiesachin <archieshah8767@gmail.com>
2024-06-02 11:10:55 +0530

    Hero movement done

--- Overworld.js
+++ Overworld.js
@@ -3,54 +3,48 @@ class Overworld{
     this.element = config.element;
     this.canvas = this.element.querySelector('.game-canvas');
     this.ctx = this.canvas.getContext("2d");
+    this.map = null; 
+  }
+
+  startGameLoop(){
+    const step = () =>{
+    
+      this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height)
+
+      //Draw Lower Layer
+      this.map.drawLowerImage(this.ctx);
+
+      //Draw game objects
+      Object.values(this.map.gameObjects).forEach(object =>{
+        object.update({
+          arrow: this.directionInput.direction
+        });
+        object.sprite.draw(this.ctx)
+      })
+
+      //Draw Upper Layer
+      this.map.drawUpperImage(this.ctx);
+
+
+      requestAnimationFrame(() => {
+        step();
+      })
+    }
+    step();
   }
 
   init(){
-    const image = new Image();   //Create a new image
-    image.onload =() =>{
-      this.ctx.drawImage(image, 0,0) //canvas has a context ctx which allows us to draw on the canvas. 0,0 is the pixels where our image is pasted. 
-    }
+    this.map = new OverworldMap(window.OverworldMap.DemoRoom)
 
-    image.src='/images/maps/DemoLower.png' //Assign source to the image
-
-    const x = 5; //variables to move the character
-    const y = 6;
+    this.directionInput = new DirectionInput()
+    this.directionInput.init()
+    
 
 
-    const shadow = new Image()
-    shadow.onload =() => {
-      this.ctx.drawImage(
-        shadow, 
-        0, //left cut
-        0, //top cut
-        32, //width of cut
-        32, //height of cut
-        x * 16 -8,
-        y *16 -18,
-        32, 
-        32
-      )
-    }
-    shadow.src ="/images/characters/shadow.png"
+    this.startGameLoop()
 
+    
 
-
-
-    const hero = new Image();
-    hero.onload =() =>{
-      this.ctx.drawImage(
-        hero, 
-        0, //left cut
-        0, //top cut
-        32, //width of cut
-        32, //height of cut
-        x * 16 -8,
-        y *16 -18,
-        32, 
-        32
-      )
-    }
-
-    hero.src = '/images/characters/people/hero.png'
+    
   }
 }
